9810f6adb2dfb8b2a6ce502506ed79cda2af9208
Kasim Sert <kasim.sert@zalando.de>
2018-04-16 11:26:45 +0200

    ok

--- src/main/java/com/techprimers/testing/FizzBuzz.java
+++ src/main/java/com/techprimers/testing/FizzBuzz.java
@@ -2,13 +2,21 @@ package com.techprimers.testing;
 
 public class FizzBuzz {
 
-    public String play(int number) {
+    public String play(final int number) {
 
-        if (number == 0) throw new IllegalArgumentException("Number must not be 0");
-        if (number % 3 == 0) return "Fizz";
-        if (number % 5 == 0) return "Buzz";
+        if (number == 0) {
+            throw new IllegalArgumentException("Number must not be 0");
+        }
 
+        if (number % 3 == 0) {
+            return "Fizz";
+        }
 
-        return String.valueOf(number);
+        if (number % 5 == 0) {
+            return "Buzz";
+        }
+
+        return String.valueOf(number + "new ");
+
     }
 }
